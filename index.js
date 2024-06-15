09aab19e65a906b80ba2a58939977c80786d82ac
c-atfish <jonlar@student.kristiania.no>
2024-06-16 01:12:20 +0200

    Added sick mouse move animation to index.html

--- index.js
+++ index.js
@@ -47,3 +47,16 @@ type();
 
 
 
+
+
+document.addEventListener('mousemove', (e) => {
+    setTimeout(() => {
+        const mouseBox = document.getElementById("mouseCircle");
+        const offsetX = mouseBox.offsetWidth / 2;
+        const offsetY = mouseBox.offsetHeight / 2;
+        mouseBox.style.left = `${e.pageX - offsetX}px`;
+        mouseBox.style.top = `${e.pageY - offsetY}px`;
+    }, 2000)
+
+})
+
