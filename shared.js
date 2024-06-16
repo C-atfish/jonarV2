4046ffa491238ab6f6bdaffe7344a185fd261715
c-atfish <jonlar@student.kristiania.no>
2024-06-16 14:50:47 +0200

    Added personalized mouse animation on all pages. Home its kotlin, about is my page, projects is linux and contact is mio

---
+++ shared.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('mousemove', (e) => {
+    setTimeout(() => {
+        const mouseBox = document.getElementById("mouseCircle");
+        const offsetX = mouseBox.offsetWidth / 2;
+        const offsetY = mouseBox.offsetHeight / 2;
+        mouseBox.style.left = `${e.pageX - offsetX}px`;
+        mouseBox.style.top = `${e.pageY - offsetY}px`;
+    }, 40)
+
+})
