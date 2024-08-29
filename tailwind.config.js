80a33ea45a84ba31d85165ca2b1e8b752d3e5999
c-atfish <jonlar@student.kristiania.no>
2024-08-29 15:42:06 +0200

    Added better overall styling to the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                // Main bg background
-                main1: '#C1AE9F',
-                main2: '#D3BDB0',
-                main3: '#89937C',
-                main4: '#69385C',
-                main5: '#715B64',
-                // For typing animation background
-                main6: '#791a41',
-                // For under menu of typing animation background. "What do i do"
-                main7: '#a42559',
-                // Foreground color
-                main8: '#88737c',
-                carousel: '#394e86',
-            }
-        }
-    }
-}
-
+  theme: {
+    extend: {
+      colors: {
+        // Main bg background
+        //main1: "#C1AE9F",
+        // BG
+        main1: "#3c3c3c",
+        main2: "#D3BDB0",
+        //main3: "#89937C",
+        main3: "#89937C",
+        // main4: '#69385C',
+        main4: "#252526",
+        // main5: '#715B64',
+        main5: "#715B64",
+        // For typing animation background
+        main6: "#791a41",
+        // For under menu of typing animation background. "What do i do"
+        main7: "#a42559",
+        // Foreground color
+        main8: "#88737c",
+        carousel: "#394e86",
+      },
+    },
+  },
+};
